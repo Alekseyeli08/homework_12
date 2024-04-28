0006f4e7ede275bf3faebe65e4d7e12cdbcd0383
Aleksey Manzhikov <sanal-manzhikov@mail.ru>
2024-04-29 00:04:49 +0300

    update str_func.py

--- str_func.py
+++ str_func.py
@@ -1,3 +1,8 @@
 def str_upper():
     text = input()
     return print(text.upper())
+
+def str_title():
+    text = input()
+    """функция для перевода первых букв в верхний регистр"""
+    return print(text.title())
